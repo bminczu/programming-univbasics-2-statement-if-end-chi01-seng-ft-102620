45b223d752badec4c421dc663f6be23be7e70100
adventurous-format-6947 <bminczu@gmail.com>
2020-09-29 20:23:20 +0000

    Automatically backed up by Learn

--- ruby_practice.rb
+++ ruby_practice.rb
@@ -1,8 +1,8 @@
 chance_of_rain = 0.6
 puts "Lets go outside!"
-if chance_of_rain > 0.5
+if chance_of_rain > 0.25
+  puts "Pack a sun shelter!"
+elsif (chnace_of_rain >0.25 && chance_of_rain<0.75)
   puts "Pack an umbrella!"
-else
-  puts "Enjoy the fine day!"
-end
-puts "Oh, and always wear sunscreen!"
+else  
+puts "Stay home bitch"
